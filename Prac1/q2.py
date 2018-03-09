5e70b0d3bf43b598c0db2820af48587fd60e2092
KeeganBentley <keegan.bentley@my.jcu.edu.au>
2018-03-09 23:18:29 +1100

    initial commit

---
+++ Prac1/q2.py
@@ -0,0 +1,1 @@
+indiciesOfTwo = [2**x for x in range(9)]
---
+++ Prac1/q3.py
@@ -0,0 +1,11 @@
+def unique_list(list_of_nums):
+    unique_num_list = []
+    for num in list_of_nums:
+        if num not in unique_num_list:
+            unique_num_list.append(num)
+            print(unique_num_list)
+        else:
+            return False
+    return True
+
+
